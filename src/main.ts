fd0a7043586396138cf59ba8618c4f565d0eda58
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2024-01-11 13:50:06 +0100

    desestructuracion

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import './style.css';
 // import "./topics/01-basic-types.ts";
-import "./topics/02-object-interface";
-import "./topics/03-functions";
-import "./topics/04-homeworrk-types";
+// import "./topics/02-object-interface";
+// import "./topics/03-functions";
+// import "./topics/04-homeworrk-types";
+// import "./topics/05-basic-desestructuring";
+import "./topics/06-function-destructuring";
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola  mundo  
 `
 console.log("Hola mundo");
+
 
--- src/topics/05-basic-desestructuring.ts
+++ src/topics/05-basic-desestructuring.ts
@@ -20,7 +20,40 @@ const audioPlayer: AudioPlayer = {
     }
 }
 
+// Objetos (desestructuración)
+// 
+// Sin desestructuración ____________________
 
+// console.log("Song: ", audioPlayer.song);
+// console.log("Duration: ", audioPlayer.songDuration);
+// console.log("Author: ", audioPlayer.details.author);
+
+
+// Con desestructuración ____________________
+
+// const song = "New Song"
+
+// const {  song: anotherSong, songDuration: duration,  details, } = audioPlayer;
+
+// const { author } = details;
+
+// console.log("Song: ", anotherSong);
+// console.log("Duration: ", duration);
+// console.log("Author: ", author);
+
+
+
+// Arrays (desestructuración)
+
+// const dbz: string [] = ["Gokú", "Vegeta", "Trunk"];
+// const trunks = dbz[3] || "No hay personaje";
+
+// console.error("Personaje 3: ", trunks);
+
+
+const[ ,  , trunks]: string [] = ["Gokú", "Vegeta", "Trunk"];
+
+console.error("Personaje 3: ", trunks);
 
 
 
